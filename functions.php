5779a29adfb41982fa98ee79d7c35403e6e552d4
Owen Ward <owenward@outlook.com>
2020-10-27 15:35:10 +0000

    Remove subscriptions

--- functions.php
+++ functions.php
@@ -7,6 +7,4 @@ require 'homepage_slideshow/slideshow_meta_setup.php';
 require 'functions_helpers/options_page.php';
 require 'functions_helpers/declare_support.php';
 require 'functions_helpers/set_menus.php';
-require 'subscriptions/setup_frontend.php';
-require 'subscriptions/handle_email_submit.php';
 require 'functions_helpers/formatInput.php';
--- subscriptions/send_email.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$headers = "From: Bloomsbury Conservation Areas <_www@hannahs-macbook-air.local>\r\n";
-$headers .= "Content-type: text/html\r\n";
-
-function send_email($recipient, $subject, $content) {
-    mail($recipient, $subject, $content, $headers);
-}
