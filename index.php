fc87ed5b30df714fc1c6b34e9e5a9ac2fd68d25b
Owen Ward <owenward@outlook.com>
2020-10-27 22:34:48 +0000

    fix faulty index page

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
-<?php
-$posts_args = Array (
-    'numberposts' => 4
-);
-$posts = get_posts($post_args);
-foreach($posts as $post) {
-    echo $post->post_title;
-};
-?>
+<?php get_header() ?>
+
+<main>
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            ?>
+            <header class='pageHeader'>
+                <h2 class='pageTitle'><?=the_title()?></h2>
+                <?=the_excerpt()?>
+            </header>
+            <article id='pageContent'>
+                <?=the_content()?>
+            </article>
+            <?php
+        }
+    }
+    ?>
+    <div id="backgroundImageDiv">
+        <?=the_post_thumbnail('full')?>
+    </div>
+</main>
+
+<?php get_footer() ?>
