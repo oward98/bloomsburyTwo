d0407b0b089b32e9aa1d6f32a2a57ba261d0f63e
Owen Ward <owenward@outlook.com>
2021-02-21 19:59:48 +0000

    Add Google Analytics

--- Components/Head/Head.php
+++ Components/Head/Head.php
@@ -3,6 +3,15 @@
 function Head() {
     ?>
     <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157499426-2"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-157499426-2');
+    </script>
+    <!-- End Google Analytics -->
     <title><?php wp_title('|', true, 'right'); bloginfo("name") ?></title>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
--- Components/HeaderNavMobile/HeaderNavMobile.js
+++ Components/HeaderNavMobile/HeaderNavMobile.js
@@ -39,6 +39,5 @@ function keepCheckOnWindowSize() {
     }
 }
 
-
 keepCheckOnWindowSize();
 addClickFunctionality();
